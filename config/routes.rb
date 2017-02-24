a77e0438f2409f865e585021a59f851a45337083
Andry Rozdolsky <drunkestpunk@gmail.com>
2017-02-24 17:42:40 +0200

    Updated version

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,6 @@ Surveyor::Engine.routes.draw do
   match '/:survey_code/:response_set_code', :to      => 'surveyor#update', :as => 'update_my_survey', :via  => :put
 
 
-  match '/:survey_code/', :to                         => 'surveyor#list', :as => 'list_survey', :via     => :get
-  match '.:survey_code/', :to                         => 'surveyor#list', :as => 'export_survey', :via     => :get
-  match '/:survey_code/:response_set_code/take', :to => 'surveyor#edit', :as   => 'edit_my_survey', :via    => :get
-  match '/:survey_code/:response_set_code', :to      => 'surveyor#update', :as => 'update_my_survey', :via  => :put
 
 
 end
